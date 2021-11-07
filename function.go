202d78155ce33a3c99a56228b04b904f59ba2d64
Agungthebesttop <agungthebesttop@gmail.com>
2021-11-07 12:06:23 +0700

    Update function.go

--- function.go
+++ function.go
@@ -2,7 +2,13 @@ package synapsemodule
 
 import "fmt"
 
-func Penjumlahan(a, b int) {
-	hasil := a + b
-	fmt.Println(hasil)
+func Greeting(hour int) {
+	if hour < 12 {
+		fmt.Println("Selamat Pagi")
+	} else if hour < 18 {
+		fmt.Println("Selamat Sore")
+	} else {
+		fmt.Println("Selamat Malam")
+	}
+	fmt.Println(hour)
 }
